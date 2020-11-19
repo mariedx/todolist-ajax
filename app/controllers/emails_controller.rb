4ee753d65fea60bef7b957235ca573e3c918ef6a
MarieDechaux <marie.dechaux@gmail.com>
2020-11-19 16:08:32 +0100

    ok suppri email ajax

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -13,4 +13,21 @@ class EmailsController < ApplicationController
     end
   end
 
+  def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
 end
