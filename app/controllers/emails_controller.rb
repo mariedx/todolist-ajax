da55f63654fd55b11d13c6871cca3f2622d88996
MarieDechaux <marie.dechaux@gmail.com>
2020-11-19 12:04:51 +0100

    ok ajax recevoir un email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,5 @@
 require 'faker'
+
 class EmailsController < ApplicationController
   def index
     @emails = Email.all
@@ -7,9 +8,9 @@ class EmailsController < ApplicationController
   def create
     @email = Email.create(object: Faker::Movie.title, body: Faker::Movie.quote)
     respond_to do |format|
-      format.html { redirect_to emails_path }
+      format.html { redirect_to root_path }
       format.js { }
     end
   end
-  
+
 end
