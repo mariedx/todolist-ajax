85baeb5d5441add843e88dc5bd5126e5a43f0741
MarieDechaux <marie.dechaux@gmail.com>
2020-11-19 11:54:14 +0100

    tentative ajax recevoir un email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,15 @@
+require 'faker'
 class EmailsController < ApplicationController
   def index
+    @emails = Email.all
   end
+
+  def create
+    @email = Email.create(object: Faker::Movie.title, body: Faker::Movie.quote)
+    respond_to do |format|
+      format.html { redirect_to emails_path }
+      format.js { }
+    end
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-3.times do
-  my_category = Category.create(title: Faker::Book.genre)
-  3.times do
-    my_task = Task.new(title: Faker::Book.title,
-                      deadline: Faker::Date.forward(days: 23),
-                      image: Faker::Avatar.image)
-    my_task.category = my_category
-    my_task.save
-  end
-end
-
-# 4.times do
-#   Email.create(object: Faker::Movie.title, body: Faker::Movie.quote)
+# 3.times do
+#   my_category = Category.create(title: Faker::Book.genre)
+#   3.times do
+#     my_task = Task.new(title: Faker::Book.title,
+#                       deadline: Faker::Date.forward(days: 23),
+#                       image: Faker::Avatar.image)
+#     my_task.category = my_category
+#     my_task.save
+#   end
 # end
+
+4.times do
+  Email.create(object: Faker::Movie.title, body: Faker::Movie.quote)
+end
